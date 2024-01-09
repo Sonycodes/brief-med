92279c97c16b99b5d532ad60f6d133ac61f75c7f
Guy-Philippe Uberos <webmaster@webdesigner.com.fr>
2024-01-09 21:34:32 +0100

    add: retrieve the values of

--- contact.php
+++ contact.php
@@ -1,4 +1,15 @@
 <?php include './utilities/header.php'; ?>
 
 
+
+<?php
+    // Récupère la valeur du paramètre 'prenom', 'nom' ... dans l'URL
+    // En utilisant la superglobale $_GET pour récupérer les valeurs des paramètres spécifiés dans l’URL de la requête HTTP 
+	// $prenom = $_GET['prenom'];
+    // $nom = $_GET['nom'];
+    // $mail = $_GET['mail'];
+    // $sujet = $_GET['sujet'];
+ 	// $message = $_GET['message'];
+?>
+
 <?php include './utilities/footer.php'; ?>
